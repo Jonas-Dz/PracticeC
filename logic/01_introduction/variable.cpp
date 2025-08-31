e2645502859a634410195bccce60a587543d6a93
Jonas-Dz <jonathandz2002@hotmail.com>
2025-08-30 23:58:48 -0500

    Commit prueba

---
+++ logic/01_introduction/variable.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Uso de variables
+int 
+double
+float
+string
+Entrada y salida de datos (terminal)
+cout << "Texto";
+tipo_var nombre (nombre de la variable que leera)
+cin >> nombre
+*/
+
+int main(){
+
+    cout << "*** Logica/Introduccion/Variables ***" << endl;
+
+    /*
+    cout << "Ejercicios Nivel 1" << endl;
+    //Entrada de edad, altura, peso e inicial y mostrar datos
+    int edad;
+    float altura;
+    double peso;
+    char inicial;
+    cout << "Ingrese su edad: ";
+    cin >> edad;
+    cout << "Ingrese su altura: ";
+    cin >> altura;
+    cout << "Ingrese su peso: ";
+    cin >> peso;
+    cout << "Ingrese la inicial de su nombre: ";
+    cin >> inicial;
+
+    cout << "--- Datos ingresados ---" << endl;
+    cout << "Inicial: " << inicial << endl;
+    cout << "Peso: " << peso << endl;
+    cout << "Altura: " << altura << endl;
+    cout << "Edad: " << edad << endl;
+    */
+
+    /*
+    cout << "Ejercicios Nivel 2" << endl;
+    //Ingreso de nombre completo, 2 numeros y operarlos, ingreso de palabras e imprimir
+    string nombre, w1, w2, w3;
+    int n1, n2;
+    cout << "Ingrese su nombre completo: ";
+    getline(cin, nombre);
+    cout << "Ingrese 2 numeros: ";
+    cin >> n1 >> n2;
+    cout << "Ingrese la palabra 1: ";
+    cin >> w1;
+    cout << "Ingrese la palabra 2: ";
+    cin >> w2;
+    cout << "Ingrese la palabra 3: ";
+    cin >> w3;
+
+    cout << "--- Datos ingresados ---" << endl;
+    cout << "Nombre completo: " << nombre << endl;
+    cout << n1 << " + " << n2 << " = " << n1+n2 << endl;
+    cout << n1 << " - " << n2 << " = " << n1-n2 << endl;
+    cout << n1 << " * " << n2 << " = " << n1*n2 << endl;
+    cout << n1 << " / " << n2 << " = " << n1/n2 << endl;
+    cout << "Palabra1: " << w1 << endl;
+    cout << "Palabra2: " << w2 << endl;
+    cout << "Palabra3: " << w3 << endl;
+    */
+
+    return 0;
+}
---
+++ logic/02_flowControl/condicionales.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Condicionales
+if(condicion ==, !=, >, <){
+    ejecucion al cumplir la condicion
+} else {
+    ejecucion al no cumplir la condicion
+}
+
+Uso de conectores logicos
+AND => && devuelve TRUE si ambos TRUE
+OR => || devuelve TRUE si al menos uno es TRUE
+NOT => ! devuelve TRUE si es FALSE, viceversa
+*/
+
+int main(){
+    cout << "*** Logica/Controles de flujo/Condicionales ***"
+    
+    return 0;
+}
